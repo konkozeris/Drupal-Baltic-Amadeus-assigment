3ae96a87f455ec65712b3bedf11a1efd938038e0
konkozeris <85123298+konkozeris@users.noreply.github.com>
2022-02-27 23:25:47 +0200

    Update EditForm.php

--- modules/custom/edit_form/input_form/src/Form/EditForm.php
+++ modules/custom/edit_form/input_form/src/Form/EditForm.php
@@ -15,103 +15,120 @@ class EditForm extends FormBase
 
     public function buildForm(array $form, FormStateInterface $form_state, $task_id = NULL)
     {
-      // get Task id from URI
-      $task_id = \Drupal::routeMatch()->getParameter('task_id');
-      $form_state->setStorage([$task_id]);
+      // user role
 
-      var_dump($form_state);
+      $user = \Drupal::currentUser();
+      $role = $user->getRoles();
 
-      // get query about Task
-      $task_connection = \Drupal::database()->query("SELECT * FROM {tasks} WHERE id = ".$task_id."");
-      $results = $task_connection->fetchAssoc();
+      $anonymous = 'anonymous';
+      $administrator = 'administrator';
+      $senior_dev = 'senior_dev';
+      $junior_dev = 'junior_dev';
 
-      //seniors names list /select//
-
-      $dev_connection = \Drupal::database()->query("SELECT `uid`, `name`
-      FROM {users_field_data}
-      INNER JOIN {user__roles}
-      ON {users_field_data}.uid = {user__roles}.entity_id
-      WHERE {users_field_data}.uid > 0 AND {user__roles}.roles_target_id = 'senior_dev'");
-
-      $senior_name_options = [];
-
-      if ($dev_connection)
+      if ($role[0] == $anonymous || $role[0] == $junior_dev)
       {
-        while ($senior_name_records = $dev_connection->fetchAssoc())
-        {
-          $senior_name_options[$senior_name_records['name']] = $senior_name_records['name'];
-        }
+        \Drupal::messenger()->addError('You have no permission to create task');
+        return;
       }
+      else if ($role[1] == $administrator || $role[1] == $senior_dev)
+      {
 
-      $form['task_name'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Task name'),
-        '#value' => $results['task_name'],
-        '#required' => TRUE,
-      ];
+        // get Task id from URI
+        $task_id = \Drupal::routeMatch()->getParameter('task_id');
+        $form_state->setStorage([$task_id]);
 
-       $form['task_url'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('URL'),
-        '#value' => $results['task_url'],
-        '#required' => TRUE,
-      ];
+        // get query about Task
+        $task_connection = \Drupal::database()->query("SELECT * FROM {tasks} WHERE id = ".$task_id."");
+        $results = $task_connection->fetchAssoc();
 
-      $form['papa_name'] = [
-        '#type' => 'select',
-        '#options' => $senior_name_options,
-        '#title' => $this->t('Name of supervisor'),
-        '#value' => $results['papa_name'],
-        '#required' => TRUE,
-      ];
+        //seniors names list /select//
 
-      $form['papa_evaluation'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Time evaluation of supervisor (hours)'),
-        '#value' => $results['papa_evaluation'],
-        '#required' => TRUE,
-      ];
+        $dev_connection = \Drupal::database()->query("SELECT `uid`, `name`
+        FROM {users_field_data}
+        INNER JOIN {user__roles}
+        ON {users_field_data}.uid = {user__roles}.entity_id
+        WHERE {users_field_data}.uid > 0 AND {user__roles}.roles_target_id = 'senior_dev'");
 
-      $form['kinder_evaluation'] = [
-        '#type' => 'textfield',
-        '#title' => $this->t('Time evaluation of junior dev (hours)'),
-        '#value' => $results['kinder_evaluation'],
-        '#required' => TRUE,
-      ];
+        $senior_name_options = [];
 
+        if ($dev_connection)
+        {
+          while ($senior_name_records = $dev_connection->fetchAssoc())
+          {
+            $senior_name_options[$senior_name_records['name']] = $senior_name_records['name'];
+          }
+        }
 
-      $form['begin_time'] = [
-        '#type' => 'date',
-        '#date_format' => 'Y-m-d H:i',
-        '#title' => $this->t('Task start time'),
-        '#value' => $results['begin_time'],
-        '#required' => TRUE,
+        $form['task_name'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Task name'),
+          '#value' => $results['task_name'],
+          '#required' => TRUE,
         ];
 
-      $form['end_time'] = [
-        '#type' => 'date',
-        '#date_format' => 'Y-m-d H:i',
-        '#title' => $this->t('Task end time'),
-        '#value' => $results['end_time'],
-        '#required' => TRUE,
-      ];
+        $form['task_url'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('URL'),
+          '#value' => $results['task_url'],
+          '#required' => TRUE,
+        ];
 
-      $form['done'] = [
-        '#type' => 'checkbox',
-        '#date_format' => 'Y-m-d H:i',
-        '#title' => $this->t('finished'),
-        '#value' => 'yes',
-      ];
+        $form['papa_name'] = [
+          '#type' => 'select',
+          '#options' => $senior_name_options,
+          '#title' => $this->t('Name of supervisor'),
+          '#value' => $results['papa_name'],
+          '#required' => TRUE,
+        ];
 
-      $form['submit'] = [
-        '#type' => 'submit',
-        '#value' => $this->t('submit')
-      ];
-      $storage = $form_state->getStorage();
-      $task_id = $storage[0];
-      var_dump($task_id);
+        $form['papa_evaluation'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Time evaluation of supervisor (hours)'),
+          '#value' => $results['papa_evaluation'],
+          '#required' => TRUE,
+        ];
 
-      return $form;
+        $form['kinder_evaluation'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Time evaluation of junior dev (hours)'),
+          '#value' => $results['kinder_evaluation'],
+          '#required' => TRUE,
+        ];
+
+
+        $form['begin_time'] = [
+          '#type' => 'date',
+          '#date_format' => 'Y-m-d H:i',
+          '#title' => $this->t('Task start time'),
+          '#value' => $results['begin_time'],
+          '#required' => TRUE,
+          ];
+
+        $form['end_time'] = [
+          '#type' => 'date',
+          '#date_format' => 'Y-m-d H:i',
+          '#title' => $this->t('Task end time'),
+          '#value' => $results['end_time'],
+          '#required' => TRUE,
+        ];
+
+        $form['done'] = [
+          '#type' => 'checkbox',
+          '#date_format' => 'Y-m-d H:i',
+          '#title' => $this->t('finished'),
+          '#value' => 'yes',
+        ];
+
+        $form['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('submit')
+        ];
+        $storage = $form_state->getStorage();
+        $task_id = $storage[0];
+        var_dump($task_id);
+
+        return $form;
+      }
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state)
